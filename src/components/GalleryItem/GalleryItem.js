6d2436fefcd0c88d9d9af6c5aa02d7f6bcd748c0
Brad Loudis <Bradley.Loudis@gmail.com>
2020-10-31 18:07:40 -0500

    adding state to GalleryItem. handlePicClick toggles isClicked boolean on state. adding conditional render

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component {
+  state = {
+    isClicked: false,
+  };
+
   handleLikeClick = (event) => {
     console.log('like btn click!');
     this.props.likebtnCallback(this.props.item.id);
   };
 
+  handlePicClick = (event) => {
+    console.log('picture click');
+    if (!this.state.isClicked) {
+      this.setState({
+        isClicked: true,
+      });
+    } else {
+      this.setState({
+        isClicked: false,
+      });
+    }
+  };
+
   render() {
+    let galleryItem = (
+      <img src={this.props.item.path} alt={this.props.item.description} />
+    );
+
+    if (this.state.isClicked) {
+      galleryItem = <p>{this.props.item.description}</p>;
+    }
+
     return (
       <div key={this.props.item.id}>
-        <img src={this.props.item.path} alt={this.props.item.description} />
-        <p>{this.props.item.description}</p>
+        <div onClick={this.handlePicClick}>{galleryItem}</div>
         <button onClick={this.handleLikeClick}>LIKE</button>
         <span> {this.props.item.likes} likes</span>
       </div>
